9ca499f200d14ee9f0a5a20270c0887fa340d224
arw327 <arw327@lehigh.edu>
2025-02-13 22:37:22 -0500

    Update adventure.py

--- adventure.py
+++ adventure.py
@@ -135,7 +135,7 @@ def enter_dungeon(player_health, current_inventory, dungeon_rooms):
         print(room_description)
         if item:
             current_inventory = acquire_item(current_inventory, item)
-        if challenge_type != "none":
+        if challenge_type is not None:
             player_health, current_inventory = handle_challenge(
                 challenge_type,
                 current_inventory,
